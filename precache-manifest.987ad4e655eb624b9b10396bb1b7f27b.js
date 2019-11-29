6216bd3de471fa8ea23ebb666a8ab53c8be920dd
Rushiraj <rushibrahmbhatt33@gmail.com>
2019-11-29 14:24:00 +0530

    Updates

---
+++ precache-manifest.987ad4e655eb624b9b10396bb1b7f27b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aebfcdde70bde16bbffcd6c8c7e8d3d",
+    "url": "/todo-react-redux/index.html"
+  },
+  {
+    "revision": "630eef83579b0ba97841",
+    "url": "/todo-react-redux/static/css/main.81221c10.chunk.css"
+  },
+  {
+    "revision": "f198dbbd21701d832879",
+    "url": "/todo-react-redux/static/js/2.3f9c2cc8.chunk.js"
+  },
+  {
+    "revision": "630eef83579b0ba97841",
+    "url": "/todo-react-redux/static/js/main.90f24935.chunk.js"
+  },
+  {
+    "revision": "e42962cf389e3f177019",
+    "url": "/todo-react-redux/static/js/runtime-main.22efbe3d.js"
+  }
+]);
